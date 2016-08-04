34186444146cc21788cd8f0fc434dcd5c33ffda1
Izzy Vivian Dupree <antispam@idupree.com>
2016-08-04 18:57:15 -0400

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,48 @@
-use std::hash::{Hash, Hasher,SipHasher};
+use std::hash::{Hash, Hasher, SipHasher};
 use std::any::Any;
 use std::rc::Rc;
 
 #[derive (Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SiphashID {
-data: [u64; 2],
+  data: [u64; 2],
 }
 pub struct SiphashID_Generator {
-data: [SipHasher; 2],
+  data: [SipHasher; 2],
 }
 impl Hasher for SiphashID_Generator {
-fn finish (& self)->u64 {panic!()}//Hack: this is actually for generating SiphashID, not 64-bit
-fn write (&mut self, bytes: & [u8]) {self.data [0].write (bytes);self.data [1].write (bytes);}
-//TODO: should we implement the others for efficiency?
+  fn finish(&self) -> u64 {
+    panic!()
+  }//Hack: this is actually for generating SiphashID, not 64-bit
+  fn write(&mut self, bytes: &[u8]) {
+    self.data[0].write(bytes);
+    self.data[1].write(bytes);
+  }
+  // TODO: should we implement the others for efficiency?
 }
 impl SiphashID_Generator {
-fn generate (& self)->SiphashID {SiphashID {data: [self.data [0].finish (), self.data [1].finish ()]}}
+  fn generate(&self) -> SiphashID {
+    SiphashID { data: [self.data[0].finish(), self.data[1].finish()] }
+  }
 }
 
 
-//TODO: Shouldn't "ordered type with minimum and maximum values" be a trait common to other situations as well? Find out if there's a standard name for that.
+// TODO: Shouldn't "ordered type with minimum and maximum values" be a trait common to other situations as well? Find out if there's a standard name for that.
 pub trait BaseTime: Clone + Ord {
-  fn min_time ()->Self;
-  fn max_time ()->Self;
+  fn min_time() -> Self;
+  fn max_time() -> Self;
 }
 impl BaseTime for i64 {
-  fn min_time ()->i64 {i64::min_value ()}
-  fn max_time ()->i64 {i64::max_value ()}
+  fn min_time() -> i64 {
+    i64::min_value()
+  }
+  fn max_time() -> i64 {
+    i64::max_value()
+  }
 }
 
 pub trait Field {
   type Data: Any;// = Self;
-  
+
   /**
   Returns a constant identifier for the type, which must be 64 bits of random data.
   
@@ -41,8 +52,8 @@ pub trait Field {
   
   TODO: change this into an associated constant once associated constants become stable.
   */
-  fn unique_identifier ()->u64;
-  
+  fn unique_identifier() -> u64;
+
   /**
   Implementors MAY return true if first and second are indistinguishable.
   
@@ -53,7 +64,9 @@ pub trait Field {
   TODO: perhaps we can create default implementations of this for POD types.
   TODO: can we create automated tests for implementations of this function?
   */
-  fn guaranteed_equal__unsafe (first: & Self::Data, second: & Self::Data)->bool {false}
+  fn guaranteed_equal__unsafe(first: &Self::Data, second: &Self::Data) -> bool {
+    false
+  }
 }
 
 #[derive (Clone, PartialEq, Eq, Hash)]
@@ -63,7 +76,7 @@ struct FieldID {
 }
 
 #[derive (Clone, PartialEq, Eq, PartialOrd, Ord)]
-struct GenericExtendedTime <Base: BaseTime> {
+struct GenericExtendedTime<Base: BaseTime> {
   base: Base,
   iteration: u64,
   id: SiphashID,
@@ -76,37 +89,38 @@ pub trait Basics: Clone {
   type Time: BaseTime;
   type Constants;
 }
-type ExtendedTime <B: Basics> = GenericExtendedTime <B::Time>;
+type ExtendedTime<B: Basics> = GenericExtendedTime<B::Time>;
 
-//Note: in the future, we expect we might use a custom hash table type that knows it can rely on SiphashID to already be random, so we don't need to hash it again. This also applies to FieldID, although there may be some complications in that case.
+// Note: in the future, we expect we might use a custom hash table type that knows it can rely on SiphashID to already be random, so we don't need to hash it again. This also applies to FieldID, although there may be some complications in that case.
 impl Hash for SiphashID {
-  fn hash <H: Hasher> (& self, state: &mut H) {self.data [0].hash (state);}
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    self.data[0].hash(state);
+  }
 }
-impl <Base: BaseTime> Hash for GenericExtendedTime <Base>{
-  fn hash <H: Hasher> (& self, state: &mut H) {self.id.hash (state);}
-}
-
-pub trait Accessor <B: Basics> {
-  fn get <F: Field> (&mut self, id: SiphashID)-> Option <& F::Data>;
-  fn constants (& self)->& B::Constants;
-}
-
-pub trait MomentaryAccessor <B: Basics>: Accessor <B> {
-  fn now (& self)->B::Time;
+impl<Base: BaseTime> Hash for GenericExtendedTime<Base> {
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    self.id.hash(state);
+  }
 }
 
+pub trait Accessor<B: Basics> {
+  fn get<F: Field>(&mut self, id: SiphashID) -> Option<&F::Data>;
+  fn constants(&self) -> &B::Constants;
+}
 
-pub trait Mutator <B: Basics>: MomentaryAccessor <B> {
-  fn get_mut <F: Field> (&mut self, id: SiphashID)->Option <&mut F::Data> where F::Data: Clone;
-  fn set <F: Field> (&mut self, id: SiphashID, data: Option <F::Data>);
-  fn random_bits (&mut self, num_bits:u32)->u64;
-  fn random_id (&mut self)->SiphashID;
+pub trait MomentaryAccessor<B: Basics>: Accessor<B> {
+  fn now(&self) -> B::Time;
 }
-pub trait PredictorAccessor <B: Basics>: Accessor <B> {
 
+
+pub trait Mutator<B: Basics>: MomentaryAccessor<B> {
+  fn get_mut<F: Field>(&mut self, id: SiphashID) -> Option<&mut F::Data> where F::Data: Clone;
+  fn set<F: Field>(&mut self, id: SiphashID, data: Option<F::Data>);
+  fn random_bits(&mut self, num_bits: u32) -> u64;
+  fn random_id(&mut self) -> SiphashID;
 }
-pub trait Snapshot <B: Basics>: MomentaryAccessor <B> {
-}
+pub trait PredictorAccessor<B: Basics>: Accessor<B> {}
+pub trait Snapshot<B: Basics>: MomentaryAccessor<B> {}
 
 pub enum FiatEventOperationResult {
   Success,
@@ -114,10 +128,10 @@ pub enum FiatEventOperationResult {
   InvalidTime,
 }
 
-pub trait TimeSteward <'a, B: Basics> {
-  type S: Snapshot <B>;
+pub trait TimeSteward<'a, B: Basics> {
+  type S: Snapshot<B>;
   type Event;
-  
+
   /**
   TimeSteward implementors are permitted, but not required, to discard old data in order to save memory. This may make the TimeSteward unusable at some points in its history.
   
@@ -125,8 +139,8 @@ pub trait TimeSteward <'a, B: Basics> {
   
   All implementors must obey certain restrictions on how other TimeSteward methods may change the result of valid_from(). Implementors may have their own methods that can alter this in customized ways, which should be documented with those individual methods.
   */
-  fn valid_from (& self)->B::Time;
-  
+  fn valid_from(&self) -> B::Time;
+
   /**
   Inserts a fiat event at some point in the history.
   
@@ -135,8 +149,12 @@ pub trait TimeSteward <'a, B: Basics> {
   steward.insert_fiat_event(time, _) must not return InvalidTime if time >= steward.valid_from().
   steward.insert_fiat_event() may not change steward.valid_from().
   */
-  fn insert_fiat_event (&mut self, time: B::Time, distinguisher: u64, event: Self::Event)->FiatEventOperationResult ;
-  
+  fn insert_fiat_event(&mut self,
+                       time: B::Time,
+                       distinguisher: u64,
+                       event: Self::Event)
+                       -> FiatEventOperationResult;
+
   /**
   Erases a fiat event that has been inserted previously.
   
@@ -145,8 +163,8 @@ pub trait TimeSteward <'a, B: Basics> {
   steward.erase_fiat_event(time, _) must not return InvalidTime if time >= steward.valid_from().
   steward.erase_fiat_event() may not change steward.valid_from().
   */
-  fn erase_fiat_event (&mut self, time: B::Time, distinguisher: u64)->FiatEventOperationResult ;
-  
+  fn erase_fiat_event(&mut self, time: B::Time, distinguisher: u64) -> FiatEventOperationResult;
+
   /**
   Returns a "snapshot" into the TimeSteward.
   
@@ -157,27 +175,27 @@ pub trait TimeSteward <'a, B: Basics> {
   steward.snapshot_before(time) must return Some if time >= steward.valid_from().
   steward.snapshot_before(time) may not increase steward.valid_from() beyond time.
   */
-  //note: we implement "before" and not "after" because we might be banning events that happen during max_time
-  fn snapshot_before (&mut self, time: B::Time)->Option <Self::S>;
+  // note: we implement "before" and not "after" because we might be banning events that happen during max_time
+  fn snapshot_before(&mut self, time: B::Time) -> Option<Self::S>;
 }
 
-pub trait FlatTimeSteward <'a, B: Basics>:TimeSteward <'a, B> {
-  fn valid_strictly_between (& self)->(B::Time, B::Time);
+pub trait FlatTimeSteward<'a, B: Basics>: TimeSteward<'a, B> {
+  fn valid_strictly_between(&self) -> (B::Time, B::Time);
 }
 
 
-type Event <M> =Rc <Fn (&mut M)>;
+type Event<M> = Rc<Fn(&mut M)>;
 #[derive (Clone)]
-enum Prediction <B: Basics, E> {
+enum Prediction<B: Basics, E> {
   Nothing,
-  Immediately (E),
-  At (B::Time, E),
+  Immediately(E),
+  At(B::Time, E),
 }
-type Predictor <B: Basics, M: Mutator <B>, PA: PredictorAccessor <B>> =Rc <for <'b, 'c> Fn (& 'b mut PA)->Prediction <B, Event <M>>>;
+type Predictor<B: Basics, M: Mutator<B>, PA: PredictorAccessor<B>> =
+  Rc<for<'b, 'c> Fn(&'b mut PA) -> Prediction<B, Event<M>>>;
 
 
 mod simple_flat_time_steward;
 
- #[test]
-fn it_works() {
-}
+#[test]
+fn it_works() {}
