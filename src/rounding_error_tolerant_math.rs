2006960bf88d2fe2b9dbec6193d3e369d237c7f0
Eli Dupree <vcs@elidupree.com>
2016-08-14 17:40:48 -0400

    More math work

--- src/rounding_error_tolerant_math.rs
+++ src/rounding_error_tolerant_math.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub, Mul, Div, Neg, Shr};
+use std::ops::{Add, Sub, Mul, Div, Neg, Shr, Shl};
 use std::cmp::{max, min};
 
 
@@ -17,9 +17,9 @@ A numeric-ish type for dealing with integer math that has possible rounding erro
 
 A Range represents an inclusive range of real-number values.
 
-(A: Range `operator` B: Range) yields a Range that is a (non-strict) superset of (for all a in A and all b in B, a `operator` b). It prefers to yield as SMALL a possible set as it can, but doesn't guarantee that. For addition, subtraction, and multiplication, this is a generalization of i64 math. For division, it is slightly different (effectively, it rounds in BOTH directions).
+(A: Range `operator` B: Range) yields a Range that is a (non-strict) superset of (for all a in A and all b in B, a `operator` b). Specifically, it chooses the smallest such Range that has integer endpoints. For addition, subtraction, and multiplication, this is a generalization of i64 math. For division, it is slightly different (effectively, it rounds in BOTH directions).
 
-Range is also a partially floating-point type: it handles overflow by increasing an exponent value and rounding off. In keeping with its strict behavior, this rounding can only INCREASE the size of the Range, not decrease it.
+Range is also a partially floating-point type: it handles overflow by increasing an exponent value and rounding off. This rounding never removes elements from the Range, but sometimes adds new ones.
 
 */
 
@@ -56,6 +56,7 @@ self.max <<= 1;
 self.exponent -= 1;
 }
 }
+pub fn includes_0 (&self)->bool {self.min <= 0 && self.max >= 0}
 }
 
 macro_rules! binary_operation_fill {
@@ -167,11 +168,17 @@ let mut result = self.clone();
 let mut other = other.clone();
 if other.min <= 0 && other.max >= 0 {return Range::everywhere()}
 if other.max <0 {other = - other; result = - result;}
-//TODO: mess with exponent 
+
+if other.exponent >result.exponent {
+result.increase_exponent_to (other.exponent);
+}
+result.exponent -= other.exponent;
+
 if result.min <0 {result.min = (result.min + 1 - other.min)/other.min;}
- else {result.min = (result.min + 1 - other.max)/other.max;}
+ else {result.min = (result.min)/other.max;}
  if result.max <0 {result.max = (result.max)/other.max;}
-  else {result.min = (result.max)/other.min;}
+  else {result.min = (result.max - 1 + other.min)/other.min;}
+result.minimize_exponent();
 result
 }
 }
@@ -196,6 +203,18 @@ result
 } 
 }
 binary_operation_fill! (Range, u32, Shr, shr);
+
+impl <'a> Shl <& 'a u32> for &'a Range {
+type Output = Range;
+fn shl (self, other: & u32)->Range {
+let mut result = self.clone();
+result.exponent += other.clone();
+result.minimize_exponent();
+result
+} 
+}
+binary_operation_fill! (Range, u32, Shl, shl);
+
 
 
 impl Range {
@@ -244,7 +263,7 @@ Some (result)
 
 A polynomial pseudo-solver, using Range.
 
-Returns a collection of ranges that include the exact roots.
+Returns a collection of ranges that include the exact roots. False-positives are possible.
 
 TODO: instead of Vec<Range>, these should return a stack-allocated type.
 
@@ -260,14 +279,14 @@ let a = terms [2];
 let b = terms [1];
 let c = terms [0];
 let discriminant = b.squared() + a*c*Range::exactly (4);
-printlnerr!(" discriminant {:?}", discriminant);
+//printlnerr!(" discriminant {:?}", discriminant);
 if discriminant.max <0 {return Vec:: new();}
 let sqrt = discriminant.sqrt().expect ("I thought we just ruled out the case where the square root would be nonexistent");
-printlnerr!(" sqrt {:?}", sqrt);
+//printlnerr!(" sqrt {:?}", sqrt);
 let result_0 = (- b - sqrt)/(a*2);
 let result_1 = (- b + sqrt)/(a*2);
-printlnerr!(" result 0 {:?}", result_0);
-printlnerr!(" result 1 {:?}", result_1);
+//printlnerr!(" result 0 {:?}", result_0);
+//printlnerr!(" result 1 {:?}", result_1);
 //if one of the results is this huge, something bad probably happened
 if result_0.exponent >0 || result_1.exponent >0 {return vec![Range::everywhere()];}
 if result_0.max >= result_1.min {
@@ -278,6 +297,33 @@ vec![result_0, result_1]
 }
 }
 
+fn find_root (terms: & [Range], min: i64, max: i64)->Option <Range> {
+let min_value = evaluate (terms, min);
+let max_value = evaluate (terms, max);
+let direction;
+if min_value.includes_0() {
+if max_value.includes_0() {return Some (Range::new (min, max));}
+direction = max_value.min.signum();
+}
+else if max_value.includes_0() {direction = - min_value.min.signum();}
+else if max_value.min.signum() == min_value.min.signum(){return None;} else {
+direction = max_value.min.signum();
+}
+
+let mut lower_bound = min;
+let mut upper_bound = max;
+let mut move_size = 1i64 << 63;
+while move_size >max - min {move_size >>= 1}
+while move_size >0 {
+if (evaluate (terms, lower_bound + move_size)*direction).max < 0 {lower_bound += move_size;}
+if (evaluate (terms, upper_bound - move_size)*direction).min > 0 {upper_bound -= move_size;}
+move_size >>= 1;
+}
+Some (Range::new (lower_bound, upper_bound))
+
+}
+fn collect_root (terms: & [Range], min: i64, max: i64, bucket: &mut Vec<Range>) {find_root (terms, min, max).map (| root | bucket.push (root));}
+
 pub fn roots (terms: & [Range])->Vec<Range> {
   match terms.len() {
     0 => vec![Range::everywhere()],
@@ -286,12 +332,38 @@ pub fn roots (terms: & [Range])->Vec<Range> {
     3 => roots_quadratic ([terms [0], terms [1], terms [2]]),
     size => {
       let derivative: Vec<Range> = terms [1..]. iter().enumerate().map (| (which, term) | term*(which as i64 + 1)).collect();
-      vec![]
+      let extrema = roots (derivative.as_slice());
+      if extrema.iter().any (| range | range.exponent >0) {return vec![Range::everywhere()];}
+      let mut results = Vec:: new();
+      if extrema.is_empty() {collect_root (terms, - i64::max_value(),i64::max_value(), &mut results);}
+      else {
+        collect_root (terms, - i64::max_value(), extrema [0].min, &mut results);
+        for which in 0..(extrema.len() - 1) {
+          results.push (extrema [which]);
+          if extrema [which].max <extrema [which +1].min {
+            collect_root (terms, extrema [which].max, extrema [which +1].min , &mut results);
+          }
+        }
+        collect_root (terms, extrema.last().unwrap().max, i64::max_value(), &mut results);
+      }
+      results
     }
   }
 }
 
+pub fn evaluate(terms: & [Range], input: i64)->Range {
+let mut factor = 1;
+let mut result = Range::exactly (0);
+for term in terms.iter() {
+result = result + (term*factor);
+factor*= input;
+}
+result
+}
+  
+
 #[test]
 fn tests() {
-printlnerr!(" {:?}", roots_quadratic ([Range::new (- 900, - 800), Range::new (500, 501), Range::exactly (50)]))
+printlnerr!(" {:?}", roots(& [Range::new (- 900, - 800), Range::new (500, 501), Range::exactly (50)]));
+printlnerr!(" {:?}", roots(& [Range::new (- 900, - 800), Range::new (500, 501), Range::exactly (50), Range::exactly (1)]));
 }
