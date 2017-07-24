4ae062e03c915c1ea87ba27ca0e6dfffc8729c9d
Eli Dupree <vcs@elidupree.com>
2017-07-24 18:58:03 -0400

    Gave a thing a better name

--- src/dynamic/thin_arc.rs
+++ src/dynamic/thin_arc.rs
@@ -6,31 +6,31 @@ use std::sync::atomic::AtomicUsize;
 use std::any::Any;
 use std::mem;
 
-struct ArcInnerAlways <List: Sublist, AlwaysData: Any> {
+struct ArcInnerCommon <List: Sublist, CommonData: Any> {
   index: ListedTypeIndex <List>,
   references: AtomicUsize,
-  data: AlwaysData,
+  data: CommonData,
 }
 #[repr(C)]
-struct ArcInner <List: Sublist, AlwaysData: Any, SpecificData: Any> {
-  always: ArcInnerAlways <List, AlwaysData>,
+struct ArcInner <List: Sublist, CommonData: Any, SpecificData: Any> {
+  common: ArcInnerAlways <List, CommonData>,
   data: SpecificData,
 }
-pub struct DynamicArc <List: Sublist, AlwaysData: Any> {
-  pointer: Shared <ArcInnerAlways <List, AlwaysData>>,
+pub struct DynamicArc <List: Sublist, CommonData: Any> {
+  pointer: Shared <ArcInnerCommon <List, CommonData>>,
 }
-pub struct TypedArc <List: Sublist, AlwaysData: Any, SpecificData: Any> {
-  pointer: Shared <ArcInner <List, AlwaysData, SpecificData>>,
+pub struct TypedArc <List: Sublist, CommonData: Any, SpecificData: Any> {
+  pointer: Shared <ArcInner <List, CommonData, SpecificData>>,
 }
 
-impl <List: Sublist, AlwaysData: Any, SpecificData: Any> TypedArc <List, AlwaysData, SpecificData> {
-  fn erase_type (self)->DynamicArc <List, AlwaysData> {
+impl <List: Sublist, CommonData: Any, SpecificData: Any> TypedArc <List, CommonData, SpecificData> {
+  fn erase_type (self)->DynamicArc <List, CommonData> {
     DynamicArc {pointer: unsafe {mem::transmute (self.pointer)}}
   }
 }
-impl <List: Sublist, AlwaysData: Any> DynamicArc <List, AlwaysData> {
+impl <List: Sublist, CommonData: Any> DynamicArc <List, CommonData> {
   /// If it's not the correct type, return the original DynamicArc 
-  fn downcast <SpecificData: Any> (self)->Result <TypedArc <List, AlwaysData, SpecificData>, DynamicArc <List, AlwaysData>> {
+  fn downcast <SpecificData: Any> (self)->Result <TypedArc <List, CommonData, SpecificData>, DynamicArc <List, CommonData>> {
     if unsafe {self.pointer.as_ref().index} == List::index:: <SpecificData> () {
       Ok (TypedArc {pointer: unsafe {mem::transmute (self.pointer)}})
     }
