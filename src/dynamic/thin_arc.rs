a38ed25e6351288fd2f4b93e1e0c91740574dc0f
Eli Dupree <vcs@elidupree.com>
2017-07-24 18:34:36 -0400

    Rename the thin_arc types
    because they're overly verbose, redundant with the module name, and
    thinness might not end up being the most notable thing about them

--- src/dynamic/thin_arc.rs
+++ src/dynamic/thin_arc.rs
@@ -6,33 +6,33 @@ use std::sync::atomic::AtomicUsize;
 use std::any::Any;
 use std::mem;
 
-struct ThinArcInnerAlways <List: Sublist, AlwaysData: Any> {
+struct ArcInnerAlways <List: Sublist, AlwaysData: Any> {
   index: ListedTypeIndex <List>,
   references: AtomicUsize,
   data: AlwaysData,
 }
 #[repr(C)]
-struct ThinArcInner <List: Sublist, AlwaysData: Any, SpecificData: Any> {
-  always: ThinArcInnerAlways <List, AlwaysData>,
+struct ArcInner <List: Sublist, AlwaysData: Any, SpecificData: Any> {
+  always: ArcInnerAlways <List, AlwaysData>,
   data: SpecificData,
 }
-pub struct DynamicThinArc <List: Sublist, AlwaysData: Any> {
-  pointer: Shared <ThinArcInnerAlways <List, AlwaysData>>,
+pub struct DynamicArc <List: Sublist, AlwaysData: Any> {
+  pointer: Shared <ArcInnerAlways <List, AlwaysData>>,
 }
-pub struct ThinArc <List: Sublist, AlwaysData: Any, SpecificData: Any> {
-  pointer: Shared <ThinArcInner <List, AlwaysData, SpecificData>>,
+pub struct TypedArc <List: Sublist, AlwaysData: Any, SpecificData: Any> {
+  pointer: Shared <ArcInner <List, AlwaysData, SpecificData>>,
 }
 
-impl <List: Sublist, AlwaysData: Any, SpecificData: Any> ThinArc <List, AlwaysData, SpecificData> {
-  fn erase_type (self)->DynamicThinArc <List, AlwaysData> {
-    DynamicThinArc {pointer: unsafe {mem::transmute (self.pointer)}}
+impl <List: Sublist, AlwaysData: Any, SpecificData: Any> TypedArc <List, AlwaysData, SpecificData> {
+  fn erase_type (self)->DynamicArc <List, AlwaysData> {
+    DynamicArc {pointer: unsafe {mem::transmute (self.pointer)}}
   }
 }
-impl <List: Sublist, AlwaysData: Any> DynamicThinArc <List, AlwaysData> {
-  /// If it's not the correct type, return the original DynamicThinArc 
-  fn downcast <SpecificData: Any> (self)->Result <ThinArc <List, AlwaysData, SpecificData>, DynamicThinArc <List, AlwaysData>> {
+impl <List: Sublist, AlwaysData: Any> DynamicArc <List, AlwaysData> {
+  /// If it's not the correct type, return the original DynamicArc 
+  fn downcast <SpecificData: Any> (self)->Result <TypedArc <List, AlwaysData, SpecificData>, DynamicArc <List, AlwaysData>> {
     if unsafe {self.pointer.as_ref().index} == List::index:: <SpecificData> () {
-      Ok (ThinArc {pointer: unsafe {mem::transmute (self.pointer)}})
+      Ok (TypedArc {pointer: unsafe {mem::transmute (self.pointer)}})
     }
     else {
       Err (self)
