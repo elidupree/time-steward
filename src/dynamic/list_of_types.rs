478391ac82a87789925945d631621f92c6dec98c
Eli Dupree <vcs@elidupree.com>
2017-07-25 11:24:49 -0400

    fixed exponential compile times

--- src/dynamic/list_of_types.rs
+++ src/dynamic/list_of_types.rs
@@ -183,30 +183,57 @@ macro_rules! Canonicalizable_tuple_impls {
 Canonicalizable_tuple_impls!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29, T30, T31);
 
 
-unsafe trait Contains <Test> {}
+unsafe trait Contains <Test> {
+  type Result;
+}
+
+struct Yes(!); struct No(!);
+
+unsafe impl <Test: Any> Contains <Test> for ! {
+  type Result = No;
+}
+unsafe impl <Test: Any, List: Uniquable> Contains <Test> for List {
+  default type Result = <List::UnprocessedTail as Contains <Test>>::Result;
+}
+unsafe impl <Test: Any, T: Any, Tail: Uniquable> Contains <Test> for (ListedType<T>, Tail) {
+  type Result = Yes;
+}
 
 unsafe trait IfContainsElse<Test, Then, Else> {
   type Result;
 }
 
-unsafe trait Unique {
+unsafe trait Uniquable {
+  type UnprocessedHead;
+  type UnprocessedTail : Uniquable;
   type Result;
 }
 
-unsafe impl <Test: Any, Then, Else, T: Any, Tail: IfContainsElse<Test, Then, Else>> IfContainsElse<Test, Then, Else> for (ListedType<T>, Tail) {
-  default type Result = <Tail as IfContainsElse<Test, Then, Else>>::Result;
+/*
+unsafe impl <Test: Any, Then, Else, List: Uniquable> IfContainsElse<Test, Then, Else> for List {
+  default type Result = <List::UnprocessedTail as IfContainsElse<Test, Then, Else>>::Result;
 }
-unsafe impl <Test: Any, Then, Else, Tail: IfContainsElse<Test, Then, Else>> IfContainsElse<Test, Then, Else> for (ListedType<Test>, Tail) {
+unsafe impl <Test: Any, Then, Else, List: Uniquable> IfContainsElse<Test, Then, Else> for List where {
   type Result = Then;
 }
 unsafe impl <Test, Then, Else> IfContainsElse<Test, Then, Else> for ! {
   type Result = Else;
+}*/
+
+unsafe impl <T: Any, Tail: Uniquable> Uniquable for (ListedType<T>, Tail) {
+  default type Result = (ListedType<T>, <Tail as Uniquable>::Result);
+  default type UnprocessedHead = ListedType<T>;
+  default type UnprocessedTail = Tail;
 }
 
-unsafe impl <T: Any, Tail: Unique + IfContainsElse<T, <Tail as Unique>::Result, (ListedType<T>, <Tail as Unique>::Result)>> Unique for (ListedType<T>, Tail) {
-  type Result = <Tail as IfContainsElse<T, <Tail as Unique>::Result, (ListedType<T>, <Tail as Unique>::Result)>>::Result;
+unsafe impl <T: Any, Tail: Uniquable> Uniquable for (ListedType<T>, Tail) where Tail: Contains <T, Result=Yes> {
+  type Result = <Tail as Uniquable>::Result;
+  type UnprocessedHead = ListedType<T>;
+  type UnprocessedTail = Tail;
 }
-unsafe impl Unique for ! {
+unsafe impl Uniquable for ! {
+  type UnprocessedHead = !;
+  type UnprocessedTail = !;
   type Result = !;
 }
 
@@ -220,13 +247,13 @@ mod tests {
   use ::dynamic;
   
   type Test = (ListedType <usize>, (ListedType <usize>, (ListedType <u64>, !)));
-  type Test2 = <Test as Unique>::Result;
+  type Test2 = <Test as Uniquable>::Result;
   
   type Test3 = (ListedType <usize>, ListedType <f32>, ListedType <f32>, ListedType <u64>, ListedType <usize>, ListedType <Vec<usize>>);
   type Test4 = ((Test3), ((Test3), Test3), Test3);
   type Test5 = <Test4 as Canonicalizable>::Result;
   type Test55 = (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<u64>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<std::vec::Vec<usize>>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<u64>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<std::vec::Vec<usize>>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<u64>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<std::vec::Vec<usize>>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<f32>, (dynamic::list_of_types::ListedType<u64>, (dynamic::list_of_types::ListedType<usize>, (dynamic::list_of_types::ListedType<std::vec::Vec<usize>>, !))))))))))))))))))))))));
-  type Test6 = <Test55 as Unique>::Result;
+  type Test6 = <Test55 as Uniquable>::Result;
   
   #[test]
   fn test_unique() {
