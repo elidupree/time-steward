e7647711e955a0d51b11c4fe1c92cd2df810c72b
Eli Dupree <vcs@elidupree.com>
2017-08-05 19:40:41 -0400

    Started writing the implementation of the real handle serialization/deserialization algorithms

--- src/rowless/serialization.rs
+++ src/rowless/serialization.rs
@@ -5,19 +5,49 @@
 //! * The serialization needs to not block other operations for more than O(1) time at a time.
 //! * The serialization must be lossless and platform-independent. For this reason, we always use bincode in low-endian mode.
 
+use std::mem;
+use std::ptr;
 
 
-
-
-struct SerializationContext <DataTimelineHandle: ???> {
-  timeline_index: HashMap<DataTimelineHandle, TimelineInfo>,
+thread_local! {
+  static DESERIALIZATION_CONTEXT: RefCell<Option <DeserializationContext>> = RefCell::new (None);
 }
 
-impl SerializationContext {
-  fn new ()->Self {
-  
+
+impl <T: DataTimeline> Serialize for DataTimelineHandle <T> {
+  fn serialize <S: Serializer> (&self, serializer: S)->Result <S::Ok, S::Error> {
+    (self.inner as usize).serialize (serializer)
   }
-  fn <T: TimeStewardSerialize> serialize (&mut self, data: & T) {
-    
+}
+
+impl <'a, T: DataTimeline> Deserialize <'a> for DataTimelineHandle <T> {
+  fn deserialize <D: Deserializer> (deserializer: D)->Result <Self, D::Error> {
+    let old_pointer = usize::deserialize (deserializer)?;
+    DESERIALIZATION_CONTEXT.with (| cell | {
+      let mut guard = cell.borrow_mut();
+      let mut map = &mut guard.unwrap().map;
+      Ok(map.entry (old_pointer).or_insert_with (| | {
+        DataTimelineHandle {
+          inner: Arc::new (unsafe {mem::uninitialized()}),
+        }
+      }).clone())
+    })
+  }
+}
+
+impl DeserializationContext {
+  fn deserialize_(deserializer: D)->Result <(), D::Error> {
+    let old_pointer = usize::deserialize (deserializer)?;
+    DESERIALIZATION_CONTEXT.with (| cell | {
+      let mut guard = cell.borrow_mut();
+      let mut map = &mut guard.unwrap().map;
+      let new_pointer = map.entry (old_pointer).or_insert_with (| | {
+        DataTimelineHandle {
+          inner: Arc::new (unsafe {mem::uninitialized()}),
+        }
+      }).inner;
+      unsafe {ptr::write (new_pointer as *mut DataTimelineHandle, DataTimeline::deserialize (D))?;}
+    })?;
+    Ok(())
   }
 }
