9e3a6f8ec1115f8a5c2ed74679a231d742afec3f
Eli Dupree <vcs@elidupree.com>
2017-09-11 22:44:01 -0400

    More work on tree_continuum

--- dev-shared/tree_continuum.rs
+++ dev-shared/tree_continuum.rs
@@ -144,7 +144,7 @@ struct SubNodeInfo<Physics: TreeContinuumPhysics> {
 }
 struct SubBoundaryInfo<Physics: TreeContinuumPhysics> {
   boundary: BoundaryHandle<Physics>,
-  old_boundary: BoundaryHandle<Physics>,
+  old_boundary: Option<BoundaryHandle<Physics>>,
   normal_dimension: usize,
   // 0-1 in all dimensions other than our the normal;
   // 0-2 in the normal
@@ -152,70 +152,16 @@ struct SubBoundaryInfo<Physics: TreeContinuumPhysics> {
   
 }
 
-/*
 
-fn split <Physics: TreeContinuumPhysics, A: EventAccessor <Steward = Physics::Steward>, N, B> (accessor: &A, node: &NodeHandle, node_initializer: N, boundary_initializer: B) where N: Fn(SubNodeInfo<Physics>)->Physics::NodeVarying, B: Fn(SubBoundaryInfo<Physics>, [Option<SubNodeInfo<Physics>>; 2])->Physics::BoundaryVarying {
+
+fn split <Physics: TreeContinuumPhysics, A: EventAccessor <Steward = Physics::Steward>, N, B> (accessor: &A, node: &NodeHandle, node_initializer: N, boundary_initializer: B) where N: Fn(SubNodeInfo<Physics>)->Physics::NodeVarying, B: Fn(SubBoundaryInfo<Physics>)->Physics::BoundaryVarying {
   let mut varying = tracking_query (accessor, &node.varying);
   //printlnerr!("{:?}", (node.width, node.center, varying.children.len()));
   //if !varying.children.is_empty() {
     //printlnerr!("{:?}", (node.width, node.center, varying.children.len()));
   //}
   assert!(varying.children.is_empty());
-  let mut old_boundaries = [[[None,None],[None,None]],[[None,None],[None,None]]];
-  let mut middle_velocities = [[0;2];2];
-  for dimension in 0.. DIMENSIONS {
-    for direction in 0..2 {
-      Face::WorldEdge => (),
-      //let direction_signum = (direction*2)-1;
-      let other_direction = (direction + 1) & 1;
-      
-      match varying.boundaries [dimension] [direction] {
-         => {
-          loop {
-            {
-              let boundary = &varying.boundaries [dimension] [direction] [0];
-              let neighbor = &boundary.nodes [direction];
-              if neighbor.width <= node.width {
-                break;
-              }
-              split (accessor, &neighbor);
-            }
-            varying = get!(accessor, &node.varying);
-          }
-          let boundary = &varying.boundaries [dimension] [direction] [0];
-          assert!(&boundary.nodes[other_direction] == node);
-          velocities [dimension] [direction] = [
-            get!(accessor, &boundary.varying).transfer_velocity,
-            get!(accessor, &boundary.varying).transfer_velocity
-          ];
-          old_boundaries [dimension] [direction] = [
-            Some(boundary.clone()),
-            Some(boundary.clone()),
-          ];
-        },
-        2 => {
-          let boundaries = &varying.boundaries [dimension] [direction];
-          assert!(&boundaries[0].nodes[other_direction] == node);
-          assert!(&boundaries[1].nodes[other_direction] == node);
-          velocities [dimension] [direction] = [
-            get!(accessor, &boundaries [0].varying).transfer_velocity,
-            get!(accessor, &boundaries [1].varying).transfer_velocity,
-          ];
-          old_boundaries [dimension] [direction] = [
-            Some(boundaries [0].clone()),
-            Some(boundaries [1].clone()),
-          ];
-        },
-        
-        _ => unreachable!(),
-      };
-    }
-    middle_velocities [dimension] = [
-      (velocities [dimension] [0] [0] + velocities [dimension] [1] [0]) / 2,
-      (velocities [dimension] [0] [1] + velocities [dimension] [1] [1]) / 2,
-    ];
-  }
-  
+
   for index in 0..(1 << DIMENSIONS) {
     let mut center = node.center;
     let mut coordinates = [0; DIMENSIONS];
@@ -229,132 +175,57 @@ fn split <Physics: TreeContinuumPhysics, A: EventAccessor <Steward = Physics::St
       }
     }
     let new_child = accessor.new_handle (NodeData {
-        width: node.width >> 1,
-        center: center,
-        parent: Some (node.clone()),
-        varying: DataTimelineCell::new (SimpleTimeline::new ()),
-      });
-  }
-  for x in 0..2 {
-    let x_signum = (x as Distance*2)-1;
-    for y in 0..2 {
-      let y_signum = (y as Distance*2)-1;
-      let center = [
-        node.center[0] + (node.width >> 2)*x_signum,
-        node.center[1] + (node.width >> 2)*y_signum,
-      ];
-      let new_child = accessor.new_handle (NodeData {
-        width: node.width >> 1,
-        center: center,
-        parent: Some (node.clone()),
-        varying: DataTimelineCell::new (SimpleTimeline::new ()),
-      });
-      set (accessor, &new_child.varying, NodeVarying {
-        last_change: *accessor.now(),
-        ink_at_last_change: varying.ink_at_last_change >> 2,
-        children: Vec::new(),
-        boundaries: [[Vec::new(), Vec::new()], [Vec::new(), Vec::new()]],
-        accumulation_rate: 0,
-        slope: [0; 2],
-      });
-      varying.children.push (new_child.clone());
-      new_children [0][x][y] = Some(new_child.clone());
-      new_children [1][y][x] = Some(new_child.clone());
-    }
-  }
-  for dimension in 0..2 {
-    for direction in 0..2 {
-      let other_direction = (direction + 1) & 1;
-      for which in 0..2 {
-        if let Some(boundary) = old_boundaries [dimension] [direction] [which].as_ref() {
-          let neighbor = &boundary.nodes [direction];
-          assert!(neighbor.width <= node.width);
-          set_with!(accessor, &neighbor.varying, | neighbor_varying | {
-            for b in neighbor_varying.boundaries [dimension] [other_direction].iter() {
-              assert!(&b.nodes[other_direction] == node);
-            }
-            neighbor_varying.boundaries [dimension] [other_direction].clear();
-          });
+      width: node.width >> 1,
+      center: center,
+      parent: Some (node.clone()),
+      varying: DataTimelineCell::new (SimpleTimeline::new ()),
+    });
+    for dimension in 0..DIMENSIONS {
+      if let Some(old_boundary) = boundary_by_coordinates_and_dimension (varying.boundaries, coordinates, dimension) {
+        let mut boundary_center = center;
+        if coordinates [dimension] == 1 {
+          boundary_center [dimension] += node.width >> 2;
         }
+        else {
+          boundary_center [dimension] -= node.width >> 2;
+        }
+        let new_boundary = accessor.new_handle (BoundaryData {
+          length: node.width >> 1,
+          center: boundary_center,
+          nodes: if direction == 0 { [neighbor.clone(), child.clone()] } else { [child.clone(), neighbor.clone()] },
+          varying: DataTimelineCell::new (SimpleTimeline::new ()),
+        });
+        let mut local_coordinates = coordinates;
+        local_coordinates [dimension] <<= 1;
+        set (accessor, new_boundary.varying, boundary_initializer (SubBoundaryInfo {
+          boundary: new_boundary,
+          old_boundary: Some(old_boundary),
+          normal_dimension: dimension,
+          local_coordinates: coordinates,
+        }));
       }
-    }
-  }
-  let mut new_boundaries = Vec::new();
-  for dimension in 0..2 {
-    let other_dimension = (dimension + 1) & 1;
-    for direction in 0..2 {
-      let other_direction = (direction + 1) & 1;
-      let direction_signum = (direction as Distance*2)-1;
-      for which in 0..2 {
-        let which_signum = (which as Distance*2)-1;
-        if let Some(boundary) = old_boundaries [dimension] [direction] [which].as_ref() {
-          let child = new_children [dimension] [direction] [which].as_ref().unwrap();
-          let neighbor = &boundary.nodes [direction];
-          let mut center = node.center;
-          center [dimension] += (node.width >> 2)*direction_signum;
-          center [other_dimension] += (node.width >> 4)*which_signum;
-          let new_boundary = accessor.new_handle (BoundaryData {
-            length: node.width >> 1,
-            center: center,
-            nodes: if direction == 0 { [neighbor.clone(), child.clone()] } else { [child.clone(), neighbor.clone()] },
-            varying: DataTimelineCell::new (SimpleTimeline::new ()),
-          });
-          set (accessor, &new_boundary.varying, BoundaryVarying {
-            transfer_velocity: velocities [dimension] [direction] [which],
-            next_change: None,
-          });
-          set_with!(accessor, &child.varying, | child_varying | {
-            child_varying.boundaries [dimension] [direction].push(new_boundary.clone());
-          });
-          set_with!(accessor, &neighbor.varying, | neighbor_varying | {
-            neighbor_varying.boundaries [dimension] [other_direction].push(new_boundary.clone());
-          });
-          new_boundaries.push (new_boundary);
-        }
-      }
-    }
-  }
-  for dimension in 0..2 {
-    let other_dimension = (dimension + 1) & 1;
-    for which in 0..2 {
-      let which_signum = (which as Distance*2)-1;
-      let child0 = new_children [dimension] [0] [which].as_ref().unwrap();
-      let child1 = new_children [dimension] [1] [which].as_ref().unwrap();
-      let mut center = node.center;
-      center [other_dimension] += (node.width >> 4)*which_signum;
-      let new_boundary = accessor.new_handle (BoundaryData {
-        length: node.width >> 1,
-        center: center,
-        nodes: [child0.clone(), child1.clone()],
-        varying: DataTimelineCell::new (SimpleTimeline::new ()),
-      });
-      set (accessor, &new_boundary.varying, BoundaryVarying {
-        transfer_velocity: middle_velocities [dimension] [which],
-        next_change: None,
-      });
-      set_with!(accessor, &child0.varying, | child0_varying | {
-        child0_varying.boundaries [dimension] [1].push(new_boundary.clone());
-      });
-      set_with!(accessor, &child1.varying, | child1_varying | {
-        child1_varying.boundaries [dimension] [0].push(new_boundary.clone());
-      });
-      new_boundaries.push (new_boundary);
-    }
-  }
-  for dimension in 0..2 {
-    for direction in 0..2 {
-      for which in 0..2 {
-        if let Some(boundary) = old_boundaries [dimension] [direction] [which].as_ref() {
-          if !just_destroyed (accessor, & boundary.varying) {
-            let boundary_varying = query (accessor, &boundary.varying);
-            destroy (accessor, &boundary.varying);
-          }
-        }
+      if coordinates [dimension] == 1 {
+        let mut boundary_center = center;
+        boundary_center [dimension] = node.center [dimension];
+        let new_boundary = accessor.new_handle (BoundaryData {
+          length: node.width >> 1,
+          center: boundary_center,
+          nodes: if direction == 0 { [neighbor.clone(), child.clone()] } else { [child.clone(), neighbor.clone()] },
+          varying: DataTimelineCell::new (SimpleTimeline::new ()),
+        });
+        let mut local_coordinates = coordinates;
+        local_coordinates [dimension] = 1;
+        set (accessor, new_boundary.varying, boundary_initializer (SubBoundaryInfo {
+          boundary: new_boundary,
+          old_boundary: Some(old_boundary),
+          normal_dimension: dimension,
+          local_coordinates: coordinates,
+        }));
       }
     }
   }
   
-  varying.boundaries = [[Vec::new(), Vec::new()], [Vec::new(), Vec::new()]];
+  varying.boundaries = ;
   
   set (accessor, &node.varying, varying);
   
@@ -455,4 +326,4 @@ fn merge <A: EventAccessor <Steward = Steward >> (accessor: &A, node: &NodeHandl
   for neighbor in neighbors {
     maybe_merge (accessor, &neighbor);
   }
-}*/
+}
