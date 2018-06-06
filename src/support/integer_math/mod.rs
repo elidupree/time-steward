b888b0cf1376077ad2e164b2744cc1b9a530fd24
Eli Dupree <vcs@elidupree.com>
2018-06-05 21:02:44 -0400

    Cleaned up a bunch of things

--- src/support/integer_math/mod.rs
+++ src/support/integer_math/mod.rs
@@ -25,17 +25,16 @@ pub fn right_shift_nicely_rounded <T: PrimInt> (input: T, shift: usize)->T {
 ///
 /// The input is represented as an integer combined with a right-shift size.
 /// For a signed integer type with B bits:
-/// Each coefficient, except the constant one, must obey coefficient.abs() < 1 << (B - 1 - shift),
-/// i.e. be small enough that `coefficient.abs() << shift` does not overflow.
-/// The constant coefficient must obey coefficient.abs() <= (1 << (B - 1)) - (1 << (B - 1 - shift)),
+/// Each coefficient, except the constant one, must obey coefficient.abs() <= T::max_value() >> shift.
+/// The constant coefficient must obey coefficient.abs() <= T::max_value() - (T::max_value() >> shift),
 /// i.e. be small enough that you can add one of the other coefficients to it without overflowing.
 /// Given these conditions, the result is guaranteed to be strictly within 1 of the ideal result.
 /// For instance, if the ideal result was 2.125, this function could return 2 or 3,
 /// but if it was 2, this function can only return exactly 2.
-pub fn evaluate_polynomial_fractional <Coefficient: Copy, T: PrimInt + Signed + From <Coefficient>> (coefficients: & [Coefficient], input: T, shift: usize)->T {
+pub fn evaluate_polynomial_at_small_input <Coefficient: Copy, T: PrimInt + Signed + From <Coefficient>> (coefficients: & [Coefficient], input: T, shift: usize)->T {
   if coefficients.len () == 0 {return T::zero()}
   let half = T::one() << (shift - 1);
-  assert!(-half <= input && input <= half, "inputs to evaluate_polynomial_fractional must be in the range [-0.5, 0.5]");
+  assert!(-half <= input && input <= half, "inputs to evaluate_polynomial_at_small_input must be in the range [-0.5, 0.5]");
   let mut result = T::zero();
   for coefficient in coefficients.iter().skip(1).rev() {
     result = right_shift_nicely_rounded ((result + (*coefficient).into())*input, shift);
@@ -46,12 +45,22 @@ pub fn evaluate_polynomial_fractional <Coefficient: Copy, T: PrimInt + Signed + 
 #[cfg (test)]
 mod tests {
   use super::*;
-  use num::FromPrimitive;
+  use num::{Zero, One, FromPrimitive};
   use num::bigint::BigInt;
   use num::rational::{Ratio, BigRational};
   use rand::distributions::Distribution;  
   use rand::distributions::range::{Range};
   use rand::{Rng, SeedableRng};
+  
+  fn evaluate_polynomial_exactly <Coefficient: Copy, T: PrimInt + Signed + From <Coefficient>> (coefficients: & [Coefficient], input: T, shift: usize)->BigRational
+    where BigInt: From <Coefficient> + From <T> {
+    let mut result: BigRational = Ratio::zero();
+    let input = Ratio::new(BigInt::from (input), BigInt::one() << shift);
+    for coefficient in coefficients.iter().rev() {
+      result = result*&input + Ratio::from_integer (BigInt::from(*coefficient));
+    }
+    result
+  }
   
   #[test]
   fn test_right_shift_nicely_rounded() {
@@ -66,13 +75,13 @@ mod tests {
   }
   
   #[test]
-  fn test_evaluate_polynomial_fractional() {
+  fn test_evaluate_polynomial_at_small_input() {
     let mut generator =::rand::chacha::ChaChaRng::from_seed ([33; 32]) ;
     for shift in 0..5 {
       let shift = 1 << shift;
       let half = 1i64 << (shift - 1);
-      let maximum = ((1u64 << (63 - shift)) - 1) as i64;
-      let constant_maximum = ((1u64 << 63) - (1u64 << (63 - shift))) as i64;
+      let maximum = i64::max_value() >> shift;
+      let constant_maximum = i64::max_value() - maximum;
       let range = Range::new_inclusive (- maximum, maximum) ;
       let constant_range = Range::new_inclusive (- constant_maximum, constant_maximum);
       let mut test_inputs = vec![- half, - half + 1, - 1, 0, 1, half - 1, half];
@@ -81,13 +90,9 @@ mod tests {
         let mut coefficients = vec![constant_range.sample (&mut generator)];
         while generator.gen() {coefficients.push (range.sample (&mut generator));}
         for input in test_inputs.iter() {
-          let result = evaluate_polynomial_fractional (& coefficients,*input, shift);
+          let result = evaluate_polynomial_at_small_input (& coefficients,*input, shift);
           
-          let mut perfect_result: BigRational = Ratio::from_integer (FromPrimitive::from_i64 (0).unwrap());
-          let perfect_input = Ratio::from_integer (BigInt::from_i64 (*input).unwrap())/Ratio::from_integer (BigInt::from_i64 (1i64).unwrap() << shift);
-          for coefficient in coefficients.iter().rev() {
-            perfect_result = perfect_result*& perfect_input + Ratio::from_integer (FromPrimitive::from_i64 (*coefficient).unwrap ());
-          }
+          let perfect_result = evaluate_polynomial_exactly (& coefficients,*input, shift);
           let difference = Ratio::from_integer (FromPrimitive::from_i64 (result).unwrap()) - perfect_result;
           assert!(difference < Ratio::from_integer (FromPrimitive::from_i64 (1).unwrap()));
         }
